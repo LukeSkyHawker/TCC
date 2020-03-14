a09ca5fe2ee46141cff35c74ce8e9f5344f5adcc
Roger Carlos de Oliveira <roger_carlos06@live.com>
2020-03-13 22:06:56 -0300

    Versão Inicial - Central Parking

---
+++ CentralParking/CentralParking/Controllers/LocadorsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CentralParking.Models;
+
+namespace CentralParking.Controllers
+{
+    public class LocadorsController : Controller
+    {
+        private CentralParkingEntities db = new CentralParkingEntities();
+
+        // GET: Locadors
+        public ActionResult Index()
+        {
+            return View(db.Locador.ToList());
+        }
+
+        // GET: Locadors/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Locador locador = db.Locador.Find(id);
+            if (locador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locador);
+        }
+
+        // GET: Locadors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locadors/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_Dono,Nome_Dono,CPF,email,usuario,senha,Tipo")] Locador locador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locador.Add(locador);
+                try
+                {
+                    db.SaveChanges();
+                } catch (DbEntityValidationException e)
+                {
+                    foreach(var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach(var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage);
+                        }
+                        
+                    }
+                    throw;
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(locador);
+        }
+
+        // GET: Locadors/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Locador locador = db.Locador.Find(id);
+            if (locador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locador);
+        }
+
+        // POST: Locadors/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_Dono,Nome_Dono,CPF,email,usuario,senha,Tipo")] Locador locador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(locador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(locador);
+        }
+
+        // GET: Locadors/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Locador locador = db.Locador.Find(id);
+            if (locador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(locador);
+        }
+
+        // POST: Locadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            Locador locador = db.Locador.Find(id);
+            db.Locador.Remove(locador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CentralParking/CentralParking/Controllers/VagasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CentralParking.Models;
+
+namespace CentralParking.Controllers
+{
+    public class VagasController : Controller
+    {
+        private CentralParkingEntities db = new CentralParkingEntities();
+
+        // GET: Vagas
+        public ActionResult Index()
+        {
+            var vagas = db.Vagas.Include(v => v.Locador);
+            return View(vagas.ToList());
+        }
+
+        // GET: Vagas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vagas vagas = db.Vagas.Find(id);
+            if (vagas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vagas);
+        }
+
+        // GET: Vagas/Create
+        public ActionResult Create()
+        {
+            ViewBag.CPF = new SelectList(db.Locador, "CPF", "Nome_Dono");
+            return View();
+        }
+
+        // POST: Vagas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_Vagas,CPF,Status,Cidade,Bairro,Rua,Numero,CEP")] Vagas vagas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vagas.Add(vagas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CPF = new SelectList(db.Locador, "CPF", "Nome_Dono", vagas.CPF);
+            return View(vagas);
+        }
+
+        // GET: Vagas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vagas vagas = db.Vagas.Find(id);
+            if (vagas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CPF = new SelectList(db.Locador, "CPF", "Nome_Dono", vagas.CPF);
+            return View(vagas);
+        }
+
+        // POST: Vagas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_Vagas,CPF,Status,Cidade,Bairro,Rua,Numero,CEP")] Vagas vagas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vagas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CPF = new SelectList(db.Locador, "CPF", "Nome_Dono", vagas.CPF);
+            return View(vagas);
+        }
+
+        // GET: Vagas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vagas vagas = db.Vagas.Find(id);
+            if (vagas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vagas);
+        }
+
+        // POST: Vagas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vagas vagas = db.Vagas.Find(id);
+            db.Vagas.Remove(vagas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
